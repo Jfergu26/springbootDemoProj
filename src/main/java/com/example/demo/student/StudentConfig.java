683858f00ea5e52e8edc8e4e1d31aecc5faa47cb
Jaryn Ferguson <Jaryntf@gmail.com>
2024-05-07 16:34:17 -0400

    adding jpa part 2. getting tables from postgres

--- src/main/java/com/example/demo/student/StudentConfig.java
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -4,12 +4,20 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @Configuration
 public class StudentConfig {
     @Bean
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
         return args -> {
+            Student jayda = new Student( "Jayda", "Jayda@studen.com", LocalDate.of(1998, Month.FEBRUARY, 27), 26);
+            Student jill = new Student( "Jill", "Jill@studen.com", LocalDate.of(1990, Month.FEBRUARY, 1), 35);
 
+            repository.saveAll(List.of(jayda,jill));
         };
     }
 }
